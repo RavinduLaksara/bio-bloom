74818cf37fb9e9c50bf694c4ca98690a5e2107ae
RavinduLaksara <laksararavindum@gmail.com>
2025-10-14 16:34:26 +0530

    Crete signup function in UserController.js

---
+++ Backend/BioBloom/controllers/UserController.js
@@ -0,0 +1,61 @@
+import User from "../models/UserSchema";
+import bcrypt from "bcrypt";
+import sgMail from "@sendgrid/mail";
+import jwt from "jsonwebtoken";
+
+// Generate OTP
+const generateOtp = () => Math.floor(100000 + Math.random() * 900000);
+
+// Sign Up
+export async function createUser(req, res) {
+  try {
+    const newUserData = req.body;
+
+    // check already sign in
+    const exitingUser = await User.findOne({ email: newUserData.email });
+    if (exitingUser) {
+      res.status(409).json({ message: "Already Sign Up. Please login..." });
+      return;
+    }
+
+    // passward salt & hash
+    const salt = await bcrypt.genSalt(10);
+    newUserData.password = await bcrypt.hash(newUserData.password, salt);
+
+    // generate otp
+    newUserData.otp = generateOtp();
+
+    // create new user
+    const newUser = new User(newUserData);
+
+    // send otp email
+    sgMail.setApiKey(process.env.SENGRID_API_KEY);
+    const msg = {
+      to: newUserData.email,
+      from: process.env.SENDGRID_VERIFIED_EMAIL,
+      templateId: "",
+      dynamic_template_data: { otp: newUserData.otp },
+    };
+    await sgMail.send(msg);
+
+    // create token
+    const token = jwt.sign(
+      {
+        email: newUserData.email,
+        name: newUserData.name,
+      },
+      process.env.JWT_SECRET,
+      { expiresIn: "5m" }
+    );
+
+    // add user
+    await newUser.save();
+    res.status(201).json({ message: "User created...", token: token });
+  } catch (e) {
+    res.status(500).json({ message: "Error", error: e });
+  }
+}
+// login
+// otp verification
+// forget password
+// reset password
