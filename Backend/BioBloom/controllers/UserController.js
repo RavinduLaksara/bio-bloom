8bf3eaa9ff572d5226bdd06a84ee5b15a84d8b99
RavinduLaksara <laksararavindum@gmail.com>
2025-10-14 22:37:00 +0530

    Create user login function in UserController.js

--- Backend/BioBloom/controllers/UserController.js
+++ Backend/BioBloom/controllers/UserController.js
@@ -33,6 +33,7 @@ export async function createUser(req, res) {
     const msg = {
       to: newUserData.email,
       from: process.env.SENDGRID_VERIFIED_EMAIL,
+      subject: "BioBloom Email Verification OTP",
       templateId: "d-c63310a7e75a47e5a31ed9c77a12aa18",
       dynamic_template_data: { otp: newUserData.otp },
     };
@@ -56,6 +57,50 @@ export async function createUser(req, res) {
   }
 }
 // login
+export async function userLogin(req, res) {
+  try {
+    const { email, passsword } = req.body;
+
+    // find user
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(409).json({ message: "User not found" });
+    }
+
+    // check password
+    const isPasswordMatch = bcrypt.compare(passsword, user.password);
+    if (!isPasswordMatch) {
+      return res
+        .status(409)
+        .json({ message: "Email or Password is incorrect" });
+    }
+
+    // create token
+    const token = jwt.sign(
+      {
+        email: user.email,
+        name: user.password,
+        role: user.role,
+      },
+      process.env.JWT_SECRET,
+      { expiresIn: "5m" }
+    );
+
+    res.status(200).json({
+      message: "Login Successfully...",
+      token: token,
+      user: {
+        email: user.email,
+        name: user.name,
+        avator: user.avator,
+      },
+    });
+  } catch (e) {
+    res.status(500).json({ message: "Error", error: e.message });
+  }
+}
 // otp verification
 // forget password
 // reset password
+
+// user - laksararavindum@gmail.com user@123
