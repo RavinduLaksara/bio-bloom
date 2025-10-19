6af155a5b97d1742c58be8cb76ed8b659ff7087a
RavinduLaksara <laksararavindum@gmail.com>
2025-10-19 17:19:08 +0530

    Create Authenticate middleware

---
+++ Backend/BioBloom/middleware/AuthMiddleware.js
@@ -0,0 +1,18 @@
+import jwt from "jsonwebtoken";
+
+export default function authToken(req, res, next) {
+  const authHeader = req.headers["authorization"];
+  const token = authHeader && authHeader.split(" ")[1];
+
+  if (!token) {
+    return res.status(409).json({ message: "Token not found" });
+  }
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = decoded;
+    next();
+  } catch (e) {
+    res.status(500).json({ message: "Error...", error: e });
+  }
+}
